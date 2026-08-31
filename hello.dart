6d96ed127a2804a82c89ddb42a14c04b436517ab
Felix Heinze <felix_heinze@teilnehmer.cdemy.de>
2026-08-31 10:11:08 +0200

    Namenseingabe , bibliothek eingebunden

--- hello.dart
+++ hello.dart
@@ -1,3 +1,9 @@
+import 'dart:io'; //Bibliothek für benutzereingaben
+
+
 void main() {
-    print('Hallo');
+    stdout.write('Bitte gib deinen Namen ein: ');
+    String? name = stdin.readLineSync(); // zeile als string einlesen
+
+    print('Hallo $name');
 }
