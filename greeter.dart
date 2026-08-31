f292c27030c103e3433f27e80b7d14f873ade4f4
Felix Heinze <felix_heinze@teilnehmer.cdemy.de>
2026-08-31 12:53:27 +0200

    schleife für alter und namen

--- greeter.dart
+++ greeter.dart
@@ -1,12 +1,29 @@
 import 'dart:io';
 
+String? vorname;
+String? nachname;
+int? alter;
 void main() {
-  stdout.write('Bitte gib deinen Vornamen ein: ');
-  String? vorname = stdin.readLineSync();
-  stdout.write('Bitte gib deinen Nachnamen ein: ');
-  String? nachname = stdin.readLineSync();
-  stdout.write('Bitte gib dein Alter ein: ');
-  String? alterEingabe = stdin.readLineSync();
-  int? alter = int.tryParse(
-      alterEingabe!); // versuch eingegebenen String in int umzuwandeln
+  // schleife für vorname und alter
+  while (vorname == null) {
+    stdout.write('Bitte gib deinen Vornamen ein: ');
+    vorname = stdin.readLineSync();
+  }
+  while (nachname == null) {
+    stdout.write('Bitte gib deinen Nachnamen ein: ');
+    nachname = stdin.readLineSync();
+  }
+
+  //schleife mit abfrage für alter
+  while (alter == null) {
+    stdout.write('Bitte gib dein Alter ein: ');
+    String? alterEingabe = stdin.readLineSync();
+    int? pruefAlter = int.tryParse(
+        alterEingabe!); // versuch eingegebenen String in int umzuwandeln
+
+    if (pruefAlter != null && pruefAlter >= 0 && pruefAlter <= 150) {
+      alter = pruefAlter;
+    } else
+      ('Bitte erneut das Alter eingeben.Passt nicht');
+  }
 }
