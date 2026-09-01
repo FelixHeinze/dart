8bc15638cbaba15080f6ba9ee193292895d55336
Felix Heinze <felix_heinze@teilnehmer.cdemy.de>
2026-09-01 07:56:22 +0200

    vorüberlegungen

---
+++ dice.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+/* //Vorüberlegung: 
+  -anzahl der würfe abfragen ( als int und gültig prüfen)
+  - würfeln bis anzahl der würfe erreicht ( schleife)
+  ---> bei 2 mal 6er wurf hintereinander, abbruch der schleife
+  ---> bei erreichen der anzahl der würfe fertig ( limit erreicht)
+-- alle würfe in liste speichern und ausgeben 
+ */
+
+void main() {}
