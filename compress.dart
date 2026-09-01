f72a9f97d1e994a649564b8f10ad91e985e6aa17
Felix Heinze <felix_heinze@teilnehmer.cdemy.de>
2026-09-01 10:09:48 +0200

    vorüberlegung

---
+++ compress.dart
@@ -0,0 +1,5 @@
+/* vorüberlegung: 
+   -string prüfen ob leer
+  - komprimierung lohnt sich erst ab 3 zeichen nacheinander ... 
+  - bei AA --> A2 keine kürzung bei AAA --> A3 kürzung vorhanden */
+  
