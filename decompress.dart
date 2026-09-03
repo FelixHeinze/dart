851ee7657b9c733d18b2dd1b86e9863b4001de9e
FelixHeinze <fheinze1993@icloud.com>
2026-09-03 13:03:05 +0200

    kommentar decompress -- überlegung für testfehler bei decompress von sonderzeichen

--- decompress.dart
+++ decompress.dart
@@ -1,21 +1,21 @@
 
 import 'dart:io';
-String decompress(String input) {
+String decompress(String input) { // funktion zum dekomprimieren eines strings
   String result = '';
   int i = 0;
 
-  while (i < input.length) {
-    String currentChar = input[i];
+  while (i < input.length) { // schleife durch die Eingabezeichenkette
+    String currentChar = input[i]; // aktuelles zeichen speichern
     i++;
-    String countStr = '';
+    String countStr = ''; // string zum speichern der anzahl der wiederholungen
 
-    while (i < input.length && RegExp(r'\d').hasMatch(input[i])) {
-      countStr += input[i];
+    while (i < input.length && RegExp(r'\d').hasMatch(input[i])) { // prüfen, ob das aktuelle zeichen eine zahl ist
+      countStr += input[i];// wenn ja, anzahl zum countStr hinzufügen
       i++;
     }
 
-    int count = countStr.isNotEmpty ? int.parse(countStr) : 1;
-    result += currentChar * count;
+    int count = countStr.isNotEmpty ? int.parse(countStr) : 1; // wenn countStr nicht leer ist, parse es zu einer zahl, sonst setze count auf 1
+    result += currentChar * count; // aktuelles zeichen count mal zum ergebnis hinzufügen
   }
 
   return result;
